284f640125b30eae767aa6ef7c14e3507ea85cb4
Jeff Hyatt <jahyatt8021@gmail.com>
2012-11-25 19:24:54 -0700

    Added a camera select UI
    Currently just switches cameras on and off.  For the checkbox to work,
    all cameras besides the main camera must start disabled.  This is more
    to showcase our UI design ideas, feel free to alter the look or layout.

--- Assets/Scripts/windowManagerScript.cs
+++ Assets/Scripts/windowManagerScript.cs
@@ -2,14 +2,33 @@ using UnityEngine;
 using System.Collections;
 
 public class windowManagerScript : MonoBehaviour {
-
+	
+	public UIPopupList planetList;
+	public GameObject currentViewer;
+	private Camera viewerCamera;
+	
 	// Use this for initialization
 	void Start () {
-	
+		currentViewer = GameObject.Find(planetList.selection+"Viewer");
+		viewerCamera = currentViewer.GetComponent<Camera>();
+		viewerCamera.enabled = true;
+		Debug.Log(viewerCamera);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	void OnPlanetSelectionChange(){
+		viewerCamera.enabled = false;
+		currentViewer = GameObject.Find(planetList.selection + "Viewer");
+		viewerCamera = currentViewer.GetComponent<Camera>();
+		viewerCamera.enabled = true;
+		Debug.Log("Selection: " + planetList.selection + "Viewer" + " Actual: " + viewerCamera);
+	}
+	
+	void OnPlanetCameraActivate(){
+		viewerCamera.enabled = !viewerCamera.enabled;	
 	}
 }
