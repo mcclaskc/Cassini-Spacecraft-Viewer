c1c79d04a37d0d9e7da8e084f6b8d0300c97326e
Jacob Rieger <jacob.rieger@colorado.edu>
2013-02-07 10:48:06 -0700

    Changed to Gimble, code cleanup

---
+++ Assets/Scripts/Gimble.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections;
+
+    public class Gimble : MonoBehaviour 
+    {
+        public Transform target; //What you're looking at
+	
+        public float distance = 10.0f; //Default distance away from target
+		public float distanceMin; //how closely your allowed to zoom
+		public float distanceMax; //how far away your allowed to zoom
+
+        public float xSpeed = 250.0f; //Panning speeds
+        public float ySpeed = 120.0f;
+
+        public float yMinLimit = -20; //Limits at how far an angle you can go over or under the object
+        public float yMaxLimit = 80;
+
+        private float x = 0.0f;
+        private float y = 0.0f;
+
+        void Start () 
+        {
+            var angles = transform.eulerAngles;
+            x = angles.y;
+            y = angles.x;
+
+        }
+
+        void Update () 
+        {
+		
+			if (target && Input.GetMouseButton(0))
+            {
+				//This is called whenever the user is holding down the mouse button
+				//Sets the x and y axis to the new rotation desired.
+				Debug.Log (Input.GetAxis ("Mouse X"));
+			
+             	setAxis();
+
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+            }
+		
+			if(target)
+			{
+				//This is called when the user is scrolling, or not doing anything
+				setTransforms();
+				distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel"), distanceMin, distanceMax);
+			
+			}
+          
+        }
+	
+		void setTransforms()
+		{
+			transform.rotation = Quaternion.Euler(y, x, 0);
+            transform.position = (Quaternion.Euler(y, x, 0)) * new Vector3(0.0f, 0.0f, -distance) + target.position;	
+		}
+	
+		void setAxis()
+		{
+			x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+		}
+
+        static float ClampAngle(float angle, float min, float max) 
+        {
+			//Keeps our angles in workable ranges
+            if (angle < -360)
+            {
+                angle += 360;
+            }
+            if (angle > 360)
+            {
+                angle -= 360;
+            }
+            return Mathf.Clamp(angle, min, max);
+        }
+    }
